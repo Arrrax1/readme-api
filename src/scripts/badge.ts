310ff829a8ae1af74fe8ac7b54b0537030cca564
Arrrax1 <bimobimo134@gmail.com>
2023-07-21 04:16:36 +0100

    svg request test

--- src/scripts/badge.ts
+++ src/scripts/badge.ts
@@ -1,21 +1,38 @@
 
-function makeBadge(badge: any): any {
-    // let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")!;
-    // svg.setAttribute('x', '0')
-    // svg.setAttribute('y', '0')
-    // svg.setAttribute('width', '150')
-    // svg.setAttribute('height', '25')
-    // let svgNS = svg.namespaceURI
-    // let rect = document.createElementNS(svgNS, 'rect')
-    // rect.setAttribute('x', '0');
-    // rect.setAttribute('y', '0');
-    // rect.setAttribute('width', "150")
-    // rect.setAttribute('height', "25")
-    // rect.setAttribute('fill', "red")
-    // svg.appendChild(rect);
-    // return svg
+interface badgeParams {
+    title: string,
+    description: string,
+    [key: string]: string
+}
 
-    return `<svg width="150" height="25" viewBox="0 0 150 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="150" height="25" fill="red"/></svg>`
+let badge: badgeParams = {
+    'title': 'label',
+    'description': 'info',
+    'title_bg': 'default',
+    'description_bg': 'default',
+    'style': 'default',
+}
+// { 'label': string, 'info': string, [key: string]: string }
+function makeBadge(query: badgeParams): string {
+    badge = {
+        ...badge,
+        ...JSON.parse(JSON.stringify(query)),
+    }
+    let label = badge.label
+    let info = badge.info
+    // in case no was not provided then default
+    let label_clr = query.label_clr == undefined || query.label_clr.split(" ").join('') == "" ? 'silver' : query.label_clr
+    let info_clr = query.info_clr == undefined || query.info_clr.split(" ").join('') == "" ? 'lightgreen' : query.info_clr
+    let des = query.des == undefined || query.des.split(" ").join('') == "" ? 'flat' : query.des
+
+    let test_svg = (`
+    <svg width="150" height="25" viewBox="0 0 150 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="150" height="25" rx="5" fill="${label_clr}"/>
+    <text x="10" y="16" fill="black" font-family="monospace">${label}</text>
+    <line x1="${label.length*7+20}" y1="-2.18557e-08" x2="${label.length*7+20}" y2="25" stroke="gray"/>
+    </svg>`)
+    return test_svg
+    // return `<svg width="105" height="22" viewBox="0 0 105 22" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="150" height="25" fill="red"/></svg>`
 }
 
 export { makeBadge }
--- src/server.ts
+++ src/server.ts
@@ -4,43 +4,29 @@ import { makeBadge } from './scripts/badge'
 const app = Express()
 const port = 3000
 
-interface badgeParams {
-    title: string,
-    description: string,
-    title_bg: string,
-    description_bg: string,
-    style: string,
-    [key: string]: string
-}
-
 app.get('/', (req, res) => {
-    res.send("not available at the moment")
+  res.send("not available at the moment")
 })
 
 app.get('/badge', (req, res) => {
-    let label = req.query.label == undefined || (<string>req.query.label).split(" ").join('') == "" ? '': (<string>req.query.label)
-    let info = req.query.info == undefined || (<string>req.query.info).split(" ").join('') == "" ? '' : (<string>req.query.info)
-    let label_clr = req.query.label_clr == undefined || (<string>req.query.label_clr).split(" ").join('') == "" ? 'default' : (<string>req.query.label_clr)
-    let info_clr = req.query.info_clr == undefined || (<string>req.query.info_clr).split(" ").join('') == "" ? 'default' : (<string>req.query.info_clr)
-    let style = req.query.style == undefined || (<string>req.query.style).split(" ").join('') == "" ? 'default' : (<string>req.query.style)
-    
-    let badge: badgeParams = {
-        'title': label,
-        'description': info,
-        'title_bg': label_clr,
-        'description_bg': info_clr,
-        'style': style,
+  // no label or info provided
+  if (req.query.label === undefined) res.send("No label provided, check your query")
+  else if (req.query.info === undefined) res.send("No info on the label provided, check your query")
+  else {
+    // label or info provided was an empty String
+    let label = req.query.label.toString().split(" ").join('')
+    let info = req.query.info.toString().split(" ").join('')
+    if (label.length < 1) res.send('False request, Check your query')
+    else if (info.length < 1) res.send('False request, Check your query')
+    // Valid query
+    else {
+      let badge = makeBadge(JSON.parse(JSON.stringify(req.query)))
+      res.setHeader('Content-Type', 'image/svg+xml');
+      res.send(badge)
     }
-
-    if (label.length<1) {
-        res.send('Must provide a Label')
-    } else if(info.length<1){
-        res.send('Must provide info on the label')
-    } else{
-        res.send(makeBadge(badge))
-    }
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Server app running at http://localhost:${port}`)
+  console.log(`Server app running at http://localhost:${port}`)
 })
